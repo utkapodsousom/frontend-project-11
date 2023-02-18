04c4a46016e36943951c4283dbe6a1381f0a9f62
utkapodsousom <utkapodsousom@gmail.com>
2023-02-18 16:37:06 +0300

    add eslint, prettier configs

--- src/app.js
+++ src/app.js
@@ -7,80 +7,77 @@ import i18next from 'i18next';
 import resources from './locales';
 import render from './render';
 
-const handleError = (error) => {
-    return error.message.key ?? 'unknown';
-};
-
-console.log(resources);
+const handleError = (error) => error.message.key ?? 'unknown';
 
 const app = async () => {
-    await i18next.init({
-        lng: 'en-US',
-        resources,
-    });
+  await i18next.init({
+    lng: 'en-US',
+    resources,
+  });
 
-    yup.setLocale({
-        string: {
-            url: () => ({ key: 'invalidUrl' }),
-        },
-        mixed: {
-            notOneOf: () => ({ key: 'alreadyInList' }),
-        },
-    });
+  yup.setLocale({
+    string: {
+      url: () => ({ key: 'invalidUrl' }),
+    },
+    mixed: {
+      notOneOf: () => ({ key: 'alreadyInList' }),
+    },
+  });
 
-    const elements = {
-        form: document.getElementById('form'),
-        inputLabel: document.querySelector('.input-label'),
-        input: document.getElementById('rss-url'),
-        submitBtn: document.getElementById('form-submit'),
-        feedback: document.querySelector('.feedback'),
-        title: document.querySelector('.title'),
-        subtitle: document.querySelector('.subtitle'),
-    };
+  const elements = {
+    form: document.getElementById('form'),
+    inputLabel: document.querySelector('.input-label'),
+    input: document.getElementById('rss-url'),
+    submitBtn: document.getElementById('form-submit'),
+    feedback: document.querySelector('.feedback'),
+    title: document.querySelector('.title'),
+    subtitle: document.querySelector('.subtitle'),
+  };
 
-    // рендерим текст из i18next 
-    document.title = i18next.t(`page.title`);
-    elements.title.textContent = i18next.t(`page.title`);
-    elements.subtitle.textContent = i18next.t(`page.subtitle`);
-    elements.inputLabel.textContent = i18next.t(`page.inputPlaceholder`);
-    elements.input.setAttribute('placeholder', i18next.t(`page.inputPlaceholder`));
-    elements.submitBtn.textContent = i18next.t(`page.addButton`);
+  // рендерим текст из i18next
+  document.title = i18next.t('page.title');
+  elements.title.textContent = i18next.t('page.title');
+  elements.subtitle.textContent = i18next.t('page.subtitle');
+  elements.inputLabel.textContent = i18next.t('page.inputPlaceholder');
+  elements.input.setAttribute('placeholder', i18next.t('page.inputPlaceholder'));
+  elements.submitBtn.textContent = i18next.t('page.addButton');
 
-    const initialState = {
-        formState: 'idle',
-        error: null,
-        feeds: [],
-    };
+  const initialState = {
+    formState: 'idle',
+    error: null,
+    feeds: [],
+  };
 
-    const watchedState = onChange(initialState, render(initialState, elements, i18next));
+  const watchedState = onChange(initialState, render(initialState, elements, i18next));
 
-    // схема валидации урла, проверяем на наличие урла в стэйте
-    const makeSchema = (addedLinks) => yup.string().required().url().notOneOf(addedLinks);
+  // схема валидации урла, проверяем на наличие урла в стэйте
+  const makeSchema = (addedLinks) => yup.string().required().url().notOneOf(addedLinks);
 
-    const handleSubmit = (event) => {
-        event.preventDefault();
+  const handleSubmit = (event) => {
+    event.preventDefault();
 
-        // получили ввод из инпута
-        const formData = new FormData(event.target);
-        const input = formData.get('url');
-        const addedLinks = watchedState.feeds.map((feed) => feed);
-        const schema = makeSchema(addedLinks);
-        schema.validate(input)
-            .then(() => {
-                watchedState.error = null;
-                watchedState.formState = 'submitting';
-            })
-            .then(() => {
-                watchedState.formState = 'added';
-                watchedState.feeds.push(input);
-            })
-            .catch((e) => {
-                watchedState.formState = 'invalid';
-                watchedState.error = handleError(e);
-            });
-    }
+    // получили ввод из инпута
+    const formData = new FormData(event.target);
+    const input = formData.get('url');
+    const addedLinks = watchedState.feeds.map((feed) => feed);
+    const schema = makeSchema(addedLinks);
+    schema
+      .validate(input)
+      .then(() => {
+        watchedState.error = null;
+        watchedState.formState = 'submitting';
+      })
+      .then(() => {
+        watchedState.formState = 'added';
+        watchedState.feeds.push(input);
+      })
+      .catch((e) => {
+        watchedState.formState = 'invalid';
+        watchedState.error = handleError(e);
+      });
+  };
 
-    elements.form.addEventListener('submit', handleSubmit);
+  elements.form.addEventListener('submit', handleSubmit);
 };
 
 export default app;
--- src/render.js
+++ src/render.js
@@ -1,45 +1,45 @@
 const renderState = (elements, value) => {
-    switch (value) {
-        case 'invalid':
-            elements.input.classList.add('is-invalid');
-            elements.feedback.classList.add('text-danger');
-            break;
-        case 'submitting':
-            elements.input.classList.remove('is-invalid');
-            elements.feedback.classList.remove('text-danger');
-            break;
-        case 'added':
-            elements.input.classList.remove('is-invalid');
-            elements.input.classList.remove('text-danger');
-            elements.feedback.textContent = '';
-            elements.form.reset();
-            elements.input.focus();
-            break;
-        default:
-            break;
-    }
+  switch (value) {
+    case 'invalid':
+      elements.input.classList.add('is-invalid');
+      elements.feedback.classList.add('text-danger');
+      break;
+    case 'submitting':
+      elements.input.classList.remove('is-invalid');
+      elements.feedback.classList.remove('text-danger');
+      break;
+    case 'added':
+      elements.input.classList.remove('is-invalid');
+      elements.input.classList.remove('text-danger');
+      elements.feedback.textContent = '';
+      elements.form.reset();
+      elements.input.focus();
+      break;
+    default:
+      break;
+  }
 };
 
 const renderError = (state, { feedback }, error, i18next) => {
-    if (error === null) {
-      return;
-    }
-    feedback.classList.add('text-danger');
-    feedback.textContent = i18next.t(`errors.${state.error}`);
-  };
+  if (error === null) {
+    return;
+  }
+  feedback.classList.add('text-danger');
+  feedback.textContent = i18next.t(`errors.${state.error}`);
+};
 
 const render = (state, elements, i18next) => (path, value) => {
-    switch (path) {
-        case 'formState':
-            renderState(elements, value, i18next);
-            break;
-        case 'error':
-            renderError(state, elements, value, i18next);
-            break;
-        default:
-            break;
-    }
-    console.log(state);
+  switch (path) {
+    case 'formState':
+      renderState(elements, value, i18next);
+      break;
+    case 'error':
+      renderError(state, elements, value, i18next);
+      break;
+    default:
+      break;
+  }
+  console.log(state);
 };
 
 export default render;
